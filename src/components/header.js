c86a19f1d80567d52ae7b120e5fba9c249015b57
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-14 12:55:53 -0400

    Added functionality to create-textElement for class

--- src/components/header.js
+++ src/components/header.js
@@ -2,5 +2,5 @@ import createTextElement from "../modules/create-textElement";
 
 export default function buildHeader() {
   const selectHeader = document.querySelector("#header");
-  selectHeader.appendChild(createTextElement("Project Overview", "h5"));
+  selectHeader.appendChild(createTextElement("Project Overview", "h5", "test"));
 }
--- src/modules/create-textElement.js
+++ src/modules/create-textElement.js
@@ -1,9 +1,17 @@
 /* Create <h1> title */
-export default function createTextElement(txtMsg, elementToCreate) {
+export default function createTextElement(
+  txtMsg,
+  elementToCreate,
+  ...elementClassArr
+) {
   const createH1Element = document.createElement(elementToCreate);
   const textNode = document.createTextNode(txtMsg);
 
   createH1Element.appendChild(textNode);
+
+  for (const arg of elementClassArr) {
+    createH1Element.classList.add(arg);
+  }
 
   return createH1Element;
 }
