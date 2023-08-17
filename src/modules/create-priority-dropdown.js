65b0da9ad540ee408d3fd6cd8f7e3da4b5d23bfc
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-08-17 09:23:37 -0400

    Added dropdownmenu functionality for priority

--- src/modules/create-priority-dropdown.js
+++ src/modules/create-priority-dropdown.js
@@ -1,4 +1,5 @@
 import createTextElement from "./create-textElement";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function createPriorityDropdown(selectDivDropDown) {
   let createDropdownButton = createTextElement(
@@ -14,7 +15,19 @@ export default function createPriorityDropdown(selectDivDropDown) {
   createDropdownButton.setAttribute("data-bs-toggle", "dropdown");
   createDropdownButton.setAttribute("aria-expanded", "false");
 
-  let createDropdownItems = createTextElement("", "ul");
-
   selectDivDropDown.appendChild(createDropdownButton);
+
+  let createDropdownMenu = createTextElement("", "ul", "dropdown-menu");
+  selectDivDropDown.appendChild(createDropdownMenu);
+
+  const menuItem = ["1", "2", "3", "*"];
+  menuItem.forEach((prio) => {
+    const listItem = document.createElement("li");
+    const anchor = document.createElement("a");
+    anchor.className = "dropdown-item";
+    anchor.href = "#";
+    anchor.textContent = prio;
+    listItem.appendChild(anchor);
+    createDropdownMenu.appendChild(listItem);
+  });
 }
