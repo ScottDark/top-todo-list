0d6fe29dd345ac7ac41cd68675054750bf4ad9bd
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2024-02-16 11:09:41 -0400

    Refactor deleteProject function to handle deletion of both project items and todo list items

--- src/modules/delete-button.js
+++ src/modules/delete-button.js
@@ -2,24 +2,33 @@ import { removeProjectById } from "./project-manager";
 import HeaderManager from "./header-manager";
 
 export default function deleteProject(deleteButton) {
-  deleteButton.addEventListener("click", function () {
+  deleteButton.addEventListener("click", function (event) {
     event.stopPropagation();
 
     const projectItem = this.closest(".project-item");
+    const toDoListItem = this.closest(".item");
 
-    const projectId = parseInt(projectItem.getAttribute("data-project-index"));
+    if (projectItem) {
+      const projectId = parseInt(
+        projectItem.getAttribute("data-project-index")
+      );
+      // Remove the project item from the sidebar
+      projectItem.remove();
+      // Remove the associated project object from the array
+      removeProjectById(projectId);
 
-    // Remove the project item from the sidebar
-    projectItem.remove();
+      const headerManager = new HeaderManager();
+      headerManager.resetHeader();
 
-    // Remove the associated project object from the array
-    removeProjectById(projectId);
-
-    const headerManager = new HeaderManager();
-    headerManager.resetHeader();
-
-    // Clear the #main of all todo list items
-    const mainElement = document.querySelector("#main");
-    mainElement.innerHTML = "";
+      // Clear the #main of all todo list items
+      const mainElement = document.querySelector("#main");
+      mainElement.innerHTML = "";
+    } else if (toDoListItem) {
+      const itemId = parseInt(toDoListItem.getAttribute("data-item-index"));
+      // Remove the todo list item
+      toDoListItem.remove();
+      // Here you should also remove the todo list item from the project's todo list
+      // removeTodoItemById(itemId);
+    }
   });
 }
